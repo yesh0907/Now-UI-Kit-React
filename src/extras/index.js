fb5cdaa4925a12758176b723a6056f4360706009
Yesh Chandiramani <yesh0907@hotmail.com>
2017-07-30 15:13:43 +0800

    Update dependencies file structure

--- src/extras/index.js
+++
@@ -1,9 +0,0 @@
-// Load all external JS files
-require('./plugins/bootstrap-switch');
-require('./plugins/nouislider.min.js');
-require('./plugins/bootstrap-datepicker');
-const nowuiKit = require('./now-ui-kit');
-import $ from 'jquery';
-$(document).ready(() => {
-    nowuiKit.default.initSliders();
-});
